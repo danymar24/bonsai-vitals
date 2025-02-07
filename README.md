61da12cd6dcb190c734333d76b982e129ef80c22
Daniel Rodriguez <danymar24@gmail.com>
2025-02-07 11:05:10 -0600

    Added moisture sensor read

--- README.md
+++ README.md
@@ -10,5 +10,22 @@ A Project to monitor a bonsai vitals using a Waveshare display and ESP32.
 * [Soil sensor](https://www.amazon.com/gp/product/B093V62VXD/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1)
 * [Water Pump](https://www.amazon.com/gp/product/B093V62VXD/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1)
 
+## Development
+
+### UI
+For the UI, I am using [SquareLine Studio](https://squareline.io/)
+
+### Arduino
+For the logic and c/cpp code, I am using VSCode and platformio.
+
+## Sensors
+
+### Soil Moisture sensor
+
+The Soil sensor is connected to the pin 6, this can be changed in the `lv_config.h` file.
+
+```h
+#define MOISTURE_SENSOR_PIN 6
+```
 
 Based on the sample of [esp-arduino-libs/ESP32_Display_Panel](https://github.com/esp-arduino-libs/ESP32_Display_Panel)
--- src/app.cpp
+++ src/app.cpp
@@ -9,15 +9,12 @@
 #include <lvgl.h>
 #include "lvgl_port_v8.h"
 
-/**
-/* To use the built-in examples and demos of LVGL uncomment the includes below respectively.
- * You also need to copy `lvgl/examples` to `lvgl/src/examples`. Similarly for the demos `lvgl/demos` to `lvgl/src/demos`.
- */
-// #include <demos/lv_demos.h>
-// #include <examples/lv_examples.h>
+#include "./ui.h";
 
 void setup()
 {
+    pinMode(MOISTURE_SENSOR_PIN, INPUT);
+
     String title = "LVGL porting example";
 
     Serial.begin(115200);
@@ -26,12 +23,7 @@ void setup()
     Serial.println("Initialize panel device");
     ESP_Panel *panel = new ESP_Panel();
     panel->init();
-#if LVGL_PORT_AVOID_TEAR
-    // When avoid tearing function is enabled, configure the RGB bus according to the LVGL configuration
-    ESP_PanelBus_RGB *rgb_bus = static_cast<ESP_PanelBus_RGB *>(panel->getLcd()->getBus());
-    rgb_bus->configRgbFrameBufferNumber(LVGL_PORT_DISP_BUFFER_NUM);
-    rgb_bus->configRgbBounceBufferSize(LVGL_PORT_RGB_BOUNCE_BUFFER_SIZE);
-#endif
+
     panel->begin();
 
     Serial.println("Initialize LVGL");
@@ -41,26 +33,7 @@ void setup()
     /* Lock the mutex due to the LVGL APIs are not thread-safe */
     lvgl_port_lock(-1);
 
-    /* Create a simple label */
-    lv_obj_t *label = lv_label_create(lv_scr_act());
-    lv_label_set_text(label, title.c_str());
-    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
-
-    /**
-     * Try an example. Don't forget to uncomment header.
-     * See all the examples online: https://docs.lvgl.io/master/examples.html
-     * source codes: https://github.com/lvgl/lvgl/tree/e7f88efa5853128bf871dde335c0ca8da9eb7731/examples
-     */
-    //  lv_example_btn_1();
-
-    /**
-     * Or try out a demo.
-     * Don't forget to uncomment header and enable the demos in `lv_conf.h`. E.g. `LV_USE_DEMO_WIDGETS`
-     */
-    // lv_demo_widgets();
-    // lv_demo_benchmark();
-    // lv_demo_music();
-    // lv_demo_stress();
+    ui_init();
 
     /* Release the mutex */
     lvgl_port_unlock();
@@ -71,5 +44,9 @@ void setup()
 void loop()
 {
     Serial.println("IDLE loop");
+
+    int moisture = analogRead(MOISTURE_SENSOR_PIN);
+    lv_label_set_text_fmt(ui_MoistureLabel, "Moisture: %d", moisture);
+
     delay(1000);
 }
